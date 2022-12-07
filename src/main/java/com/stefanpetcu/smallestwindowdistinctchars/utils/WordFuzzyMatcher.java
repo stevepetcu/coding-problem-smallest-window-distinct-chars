4c9c94b8f615faed261a816018034a0d55e3f72b
Stefan-Daniel Petcu <hello@stefanpetcu.com>
2022-12-07 19:34:22 +0000

    (SP) Fix comment consistency.

--- src/main/java/com/stefanpetcu/smallestwindowdistinctchars/utils/WordFuzzyMatcher.java
+++ src/main/java/com/stefanpetcu/smallestwindowdistinctchars/utils/WordFuzzyMatcher.java
@@ -23,7 +23,7 @@ public class WordFuzzyMatcher {
 
         // TODO: starting from a random index & direction for "rolling" the window might make this faster.
         //  We would have to ensure the window stays within bounds and remember the starting index + reverse
-        //  the sliding direction to ensure a full pass of the input chars.
+        //  the rolling direction to ensure a full pass of the input chars.
         //  For now, we always start with the leftmost character and roll the window to the right.
         var rollingWindowStartingIndex = 0;
         var rollingWindowSize = uniqueWordChars.size();
